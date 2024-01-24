c4563749144cb8becb2f473c01fe769f1861e91e
Luke Withers <128609004+Lwithers93@users.noreply.github.com>
2024-01-24 16:06:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 var stepCount = 0;
 
-var countDisplay = document.querySelector("#stepCount").innerHTML;
+var countDisplay = document.querySelector("#stepCount");
 
-document.addEventListener("devicemotion", () => {
-  stepCount += 1;
-  countDisplay = stepCount;
-});
+// document.addEventListener("devicemotion", () => {
+//   stepCount += 1;
+//   countDisplay = stepCount;
+// });
+
+var x;
+var y;
+var z;
+
+function handleMotionEvent(event) {
+  var newX = event.accelerationIncludingGravity.x;
+  var newY = event.accelerationIncludingGravity.y;
+  var newZ = event.accelerationIncludingGravity.z;
+
+  if ((newX !== x) | (newY !== y) | (newZ !== z)) {
+    stepCount += 1;
+    countDisplay = stepCount;
+  }
+  console.log(x, y, z);
+}
+
+window.addEventListener("devicemotion", handleMotionEvent, true);
+
+// setInterval(() => {
+//   stepCount += 1;
+//   countDisplay.innerHTML = stepCount;
+// }, 1000);
