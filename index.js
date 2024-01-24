6dc5071bbfffefb15e9fe2377f14b61a6524e292
Luke Withers <128609004+Lwithers93@users.noreply.github.com>
2024-01-24 14:26:06 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var stepCount = 0;
 
 var countDisplay = document.querySelector("#stepCount").innerHTML;
 
-document.addEventListener("devicemotion", (event) => {
+document.addEventListener("devicemotion", () => {
   stepCount += 1;
   countDisplay = stepCount;
 });
